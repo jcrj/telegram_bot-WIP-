7943d8090d0bb9949307ecd74e27246eb93541d5
jcrj <jovanchuarj99@gmail.com>
2022-05-05 01:34:34 +0800

    new changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telegram.ext
+import pandas_datareader as web
 
 with open('token.txt', 'r') as f:
     TOKEN = f.read()
@@ -6,12 +7,20 @@ with open('token.txt', 'r') as f:
 def start(update, context):
     update.message.reply_text("Hello, welcome to my first bot!")
 
+def stock(update, context):
+    ticker = context.args[0] #args passed to the stock command eg. /stock AAPL (assumes user passes something valid)
+    data = web.DataReader(ticker, 'yahoo')
+    price = data.iloc[-1]['Close']
+    update.message.reply_text(f'The current price of {ticker} is ${price:.2f}.')
+
+
 def help(update, context):
     update.message.reply_text("""
     The following commands are available:
 
     /start --- starts the bot
     /help --- this exact message
+    /stock --- find latest price of XXX stock
     """)
 
 updater = telegram.ext.Updater(TOKEN, use_context = True)
@@ -19,6 +28,7 @@ disp = updater.dispatcher
 
 disp.add_handler(telegram.ext.CommandHandler("start", start))
 disp.add_handler(telegram.ext.CommandHandler("help", help))
+disp.add_handler(telegram.ext.CommandHandler("stock", stock))
 
 updater.start_polling()
 updater.idle()
